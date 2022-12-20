3dab5edf0536188d3c3f529204997de193bff74e
Haniel99 <aniballaura8@gmail.com>
2022-12-20 08:50:56 -0300

    cancelar cancelado

--- pruebas/modelo/cancelar.php
+++ pruebas/modelo/cancelar.php
@@ -8,14 +8,7 @@ if (file_exists("venta.txt")) {
         $valor = fgets($archivo);
     } while (!feof($archivo));
 
-    if (!empty($valor)) {
-        $sql = "
-        UPDATE productos_tienda pt1
-        set cantidad = (select (pt.cantidad + vp.cantidad) total from productos_tienda pt join venta_producto vp on(pt.producto_id = vp.producto_id and 		pt.tienda_id = vp.tienda_id) ) 
-        where EXISTS (select 1 from venta_producto where producto_id = pt1.producto_id and tienda_id = pt1.tienda_id and venta_id = '".$valor."')
-        ";
-        mysqli_query($conexion, $sql);
-        
+    if (!empty($valor)) {        
         $sql = "
         delete from venta 
         where venta_id = '".$valor."'
