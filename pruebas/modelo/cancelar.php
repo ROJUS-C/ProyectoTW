a64f0b2cc7a7c82a6157dd5f47eeb589779e07b7
Haniel99 <aniballaura8@gmail.com>
2022-12-20 00:48:27 -0300

    sumar al cancelar

--- pruebas/modelo/cancelar.php
+++ pruebas/modelo/cancelar.php
@@ -10,6 +10,13 @@ if (file_exists("venta.txt")) {
 
     if (!empty($valor)) {
         $sql = "
+        UPDATE productos_tienda pt1
+        set cantidad = (select (pt.cantidad + vp.cantidad) total from productos_tienda pt join venta_producto vp on(pt.producto_id = vp.producto_id and pt.tienda_id = vp.tienda_id and vp.venta_id = '".$valor."') 
+        where vp.producto_id = pt1.producto_id and vp.tienda_id = pt1.tienda_id); 
+        ";
+        mysqli_query($conexion, $sql);
+        
+        $sql = "
         delete from venta 
         where venta_id = '".$valor."'
         ";
