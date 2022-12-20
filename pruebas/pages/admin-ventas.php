2e3f0b0835d50d7dc27cef70c09bc6f13b988f33
ROJUS-C <rodrigo.suana.c@gmail.com>
2022-12-20 00:15:15 -0300

    seccion-ventas admin

--- pruebas/pages/admin-ventas.php
+++ pruebas/pages/admin-ventas.php
@@ -1,9 +1,24 @@
 <?php
+    include_once '../modelo/conexion.php';
 
     session_start();
     if(!isset($_SESSION['acceso']) || $_SESSION['acceso'] == false){
         die("Te has intentado colar en la aplicacion principal");
     }
+
+    $sqlVentas = "
+    SELECT t.nombre nombreTienda, u.nombre, u.apellido, v.venta_id, v.precio, v.fecha FROM venta v
+    JOIN usuarios u ON v.usuario_id = u.usuario_id
+    JOIN tiendas t ON u.tienda = t.tienda_id
+    WHERE u.usuario_id IN (
+        SELECT u.usuario_id FROM usuarios u
+        JOIN tiendas t on u.tienda = t.tienda_id
+        JOIN usuarios a on t.administrador = a.usuario_id
+        WHERE t.administrador = '".$_SESSION['usuario_id']."'
+        )
+    ORDER BY v.fecha DESC
+    ";
+    $resultadoVentas = mysqli_query($conexion, $sqlVentas);
 
 ?>
 
@@ -48,6 +63,98 @@
                 include "componentes/encabezado.php";
 
                 ?>
+                <div class="container-fluid">
+                    <?php 
+                                
+                    if(isset($_GET['venta_id'])){
+
+                        $sqlProductosVenta = "
+                        SELECT p.nombre, p.precio, vp.cantidad, vp.precio total FROM venta v
+                        JOIN venta_producto vp ON v.venta_id = vp.venta_id
+                        JOIN productos p ON vp.producto_id = p.producto_id
+                        WHERE v.venta_id = '".$_GET['venta_id']."'
+                        ";
+                        $resultadoProductosVenta = mysqli_query($conexion, $sqlProductosVenta);
+
+                    ?>
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3 d-flex align-items-center justify-content-between" style="background-color: var(--color-main);">
+                            <h6 class="m-0 font-weight-bold text-white d-inline">Lista de Productos</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Producto</th>
+                                            <th>Precio</th>
+                                            <th>Cantidad</th>
+                                            <th>Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                            foreach($resultadoProductosVenta as $valor){
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $valor['nombre']; ?></td>
+                                            <td>$ <?php echo $valor['precio']; ?></td>
+                                            <td><?php echo $valor['cantidad']; ?></td>
+                                            <td><?php echo $valor['total']; ?></td>
+                                        </tr>
+                                        
+                                        <?php } ?>
+
+                                    </tbody>
+                                </table>
+                                <div class="d-flex justify-content-center mt-3">
+                                    <a href="http://localhost/proyectTW/pruebas/pages/admin-ventas.php" class="btn text-white mx-1 mt-2" style="background-color: var(--color-main);">Volver</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php } else{?>
+
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3 d-flex align-items-center justify-content-between" style="background-color: var(--color-main);">
+                            <h6 class="m-0 font-weight-bold text-white d-inline">Historial de Ventas</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Tienda</th>
+                                            <th>Empleado</th>
+                                            <th>Precio</th>
+                                            <th>Fecha</th>
+                                            <th>Opciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                            foreach($resultadoVentas as $valor){
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $valor['nombreTienda']; ?></td>
+                                            <td><?php echo $valor['nombre'] .' '. $valor['apellido']; ?></td>
+                                            <td>$ <?php echo $valor['precio']; ?></td>
+                                            <td><?php echo $valor['fecha']; ?></td>
+                                            <td>
+                                                <a href="http://localhost/proyectTW/pruebas/pages/admin-ventas.php?venta_id=<?php echo $valor['venta_id'] ?>" class="btn" style="background-color: var(--color-main); color: white;">Ver productos</a>
+                                            </td>
+                                        </tr>
+                                        
+                                        <?php } ?>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <?php } ?>
+                    
+                </div>
             </div>    
         </div>
 
